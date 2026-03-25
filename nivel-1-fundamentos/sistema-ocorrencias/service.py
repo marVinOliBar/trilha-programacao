c4b3c3e35b88022ce05cd439e960d02ed3ed348c
Marcus Vinicius Barbosa <80490228+marVinOliBar@users.noreply.github.com>
2026-03-25 11:03:39 -0300

    Update service.py

--- nivel-1-fundamentos/sistema-ocorrencias/service.py
+++ nivel-1-fundamentos/sistema-ocorrencias/service.py
@@ -1,7 +1,23 @@
 import storage
-from logica import adicionar_ocorrencia
+from logica import (
+    adicionar_ocorrencia,
+    atualizar_ocorrencia,
+)
 
 def registrar_ocorrencia_service(ocorrencias, tipo, local, descricao):
     ocorrencia = adicionar_ocorrencia(ocorrencias, tipo, local, descricao)
     storage.salvar(ocorrencias)
     return ocorrencia
+
+def editar_ocorrencia_service(ocorrencias, indice, novo_tipo, novo_local, nova_descricao):
+    if not novo_tipo:
+        print()
+    elif not novo_local:
+        print()
+    elif nova_descricao:
+        print()
+    else:
+        ocorrencia = atualizar_ocorrencia(ocorrencias, indice, novo_tipo, novo_local, nova_descricao)
+        storage.salvar(ocorrencias)
+        return (True, ocorrencia)
+        
